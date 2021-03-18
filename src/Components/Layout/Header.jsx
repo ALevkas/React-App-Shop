f7bfe30d7dfef3a09fdbe6f11a39af3599fca2b0
ailevchenko <levchenko.ai305@gmail.com>
2021-03-18 22:22:52 +0300

    Add other

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -2,12 +2,15 @@ const Header = () => {
     return (
         <nav className='green darken-1'>
             <div className='nav-wrapper'>
-                <a href='#' className='brand-logo'>
-                    Movies finder
+                <a
+                    href='https://github.com/ALevkas/react-shop'
+                    className='brand-logo'
+                >
+                    Shop fortnite
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
-                        <a href='#'>My other app</a>
+                        <a href='https://github.com/ALevkas/'>My other app</a>
                     </li>
                 </ul>
             </div>
